c0d455b5459160047c24b23c375694f0303a3dd1
jackiepo <20110759+jroquezzz@users.noreply.github.com>
2021-07-07 14:01:31 +1000

    new change

--- our_bot.py
+++ our_bot.py
@@ -5,7 +5,7 @@ class CompetitorInstance():
         self.trueValue = -1
         self.should_bid = True
         self.our_lastBid = 0
-        self.penalty = 100
+        self.penalty = 50
         pass
     
     def onGameStart(self, engine, gameParameters):
@@ -36,21 +36,29 @@ class CompetitorInstance():
         # lastBid is the last bid that was made
         mean = self.gameParameters["meanTrueValue"]
         stdv = self.gameParameters["stddevTrueValue"]
-        our_bid = lastBid + self.minbid + 1
+        #our_bid = lastBid + self.minbid + 1
         if(lastBid == self.our_lastBid):
             return
-        if(self.trueValue != -1):
-            if(lastBid <= self.trueValue/2):
-                self.engine.makeBid(our_bid)
-                self.engine.print(f"We made a bid for {our_bid} and the true val is: {self.trueValue}")
-                self.our_lastBid = our_bid
-                return
-        if (lastBid < mean - abs(stdv)):
-            # But don't bid too high!
-            self.engine.makeBid(lastBid+self.minbid + 1)
-            self.engine.print(f"We made a bid for {our_bid}")
+        
+        our_min_bid = last_bid + self.minbid + 1
+
+        sd_025_range = range(mean - 0.25*stdv, mean + 0.25*stdv)
+        sd_05_range = range(mean - 0.5*stdv, mean + 0.5*stdv)
+        sd_075_range = range(mean -0.75*stdv, mean + 0.75*stdv)
+        if(self.trueValue != -1): #know true value
+            if self.trueValue in sd_025_range:
+                our_bid = random.randint(our_min_bid, range(mean-stdv,mean-0.25*stdv))
+            if self.trueValue in sd_05_range - sd_025_range:
+                our_bid = random.randint(our_min_bid, range(mean-stdv,mean-0.5*stdv))
+            if self.trueValue in sd_075_range - sd_075_range:
+                our_bid = random.randint(our_min_bid, range(mean-stdv,mean-0.75*stdv))
+
+            self.engine.makebid(our_bid)
+            self.engine.print(f"We made a bid for {our_bid} and the true value is: {self.trueValue}")
             self.our_lastBid = our_bid
-        pass
+
+        if(self.trueValue == -1): # don't know true value
+            pass
 
     def onAuctionEnd(self):
         # Now is the time to report team members, or do any cleanup.
