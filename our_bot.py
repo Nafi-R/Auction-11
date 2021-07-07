2eee4b8e994be8f0d7f536f865a81e20a76e41d0
jackiepo <20110759+jroquezzz@users.noreply.github.com>
2021-07-07 16:34:01 +1000

    math function added

--- our_bot.py
+++ our_bot.py
@@ -5,9 +5,13 @@ class CompetitorInstance():
         self.trueValue = -1
         self.should_bid = True
         self.our_lastBid = 0
-        self.penalty = 50
+        self.prevBid = 0
+        self.hasBid = False 
+        self.has_made_first_bid = [] 
+        self.ourbots = []
+        self.competitor_bots = []
         pass
-    
+
     def onGameStart(self, engine, gameParameters):
         # engine: an instance of the game engine with functions as outlined in the documentation.
         self.engine=engine
@@ -29,33 +33,72 @@ class CompetitorInstance():
     def onBidMade(self, whoMadeBid, howMuch):
         # whoMadeBid is the index of the player that made the bid
         # howMuch is the amount that the bid was
+        
+        #identify who made their first bid
+        # if bid is made, check if equal to our first bid by inversing the math function
+        # if not our bot, check is NPC bot
+        # if not NPC bot, add to competitor list
+        # identify who didn't make a bid 
+
+        if whoMadeBid not in self.has_made_first_bid:
+            temp = self.prevBid
+            if howMuch == self.math_func(temp):
+                self.ourbots.append(whoMadeBid)
+                self.engine.print(f"Our bots are {self.ourbots}")
+            else:
+                self.competitor_bots.append(whoMadeBid)
+                self.engine.print(f"Our competitors bots are {self.competitor_bots}")
+            
+            self.has_made_first_bid.append(whoMadeBid)
+        
         self.engine.print(f"Someone at position [{whoMadeBid}] made a bid for (${howMuch})")
         pass
+    
+
+    def math_func(self,lastBid) -> int:
+        last_digit = (lastBid+8)%10
+        power_digit = last_digit^2
+        self.prevBid = (lastBid+8) + power_digit
+        return self.prevBid
+        
+        #lastbid = 2000
+        # minbid = 2000 + 8 = 2008
+        #last_digit = 8
+        #power_digit = last_digit^2 (64)
+        #prevBid = (lastBid+8) + power_digit (2072)
+        #prevBid = 2072
 
     def onMyTurn(self,lastBid):
         # lastBid is the last bid that was made
         mean = self.gameParameters["meanTrueValue"]
         stdv = self.gameParameters["stddevTrueValue"]
-        #our_bid = lastBid + self.minbid + 1
-        if(lastBid == self.our_lastBid):
-            return
+
+        #run only on first bid to identify bots
+        if self.hasBid == False:
+            self.engine.makeBid(math_func(lastBid))
         
-        our_min_bid = last_bid + self.minbid + 1
 
-        sd_025_range = range(mean - 0.25*stdv, mean + 0.25*stdv)
-        sd_05_range = range(mean - 0.5*stdv, mean + 0.5*stdv)
-        sd_075_range = range(mean -0.75*stdv, mean + 0.75*stdv)
-        if(self.trueValue != -1): #know true value
-            if self.trueValue in sd_025_range:
-                our_bid = random.randint(our_min_bid, range(mean-stdv,mean-0.25*stdv))
-            if self.trueValue in sd_05_range - sd_025_range:
-                our_bid = random.randint(our_min_bid, range(mean-stdv,mean-0.5*stdv))
-            if self.trueValue in sd_075_range - sd_075_range:
-                our_bid = random.randint(our_min_bid, range(mean-stdv,mean-0.75*stdv))
+        #our_bid = lastBid + self.minbid + 1
+        
+        #if(lastBid == self.our_lastBid):
+        #    return
+        
+        #our_min_bid = lastBid + self.minbid + 1
 
-            self.engine.makebid(our_bid)
-            self.engine.print(f"We made a bid for {our_bid} and the true value is: {self.trueValue}")
-            self.our_lastBid = our_bid
+        #sd_025_range = range(mean - 0.25*stdv, mean + 0.25*stdv)
+        #sd_05_range = range(mean - 0.5*stdv, mean + 0.5*stdv)
+        #sd_075_range = range(mean -0.75*stdv, mean + 0.75*stdv)
+        #if(self.trueValue != -1): #know true value
+         #   if self.trueValue in sd_025_range:
+          #      our_bid = random.randint(our_min_bid, range(mean-stdv,mean-0.25*stdv))
+           # if self.trueValue in sd_05_range - sd_025_range:
+            #    our_bid = random.randint(our_min_bid, range(mean-stdv,mean-0.5*stdv))
+            #if self.trueValue in sd_075_range - sd_075_range:
+             #   our_bid = random.randint(our_min_bid, range(mean-stdv,mean-0.75*stdv))
+
+            # self.engine.makebid(our_bid)
+            # self.engine.print(f"We made a bid for {our_bid} and the true value is: {self.trueValue}")
+            # self.our_lastBid = our_bid
 
         if(self.trueValue == -1): # don't know true value
             pass
